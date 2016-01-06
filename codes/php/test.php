36863bcf9391cb9739ab16e7f59db90fe1fd41fc
royaso <zhangroyaso@gmail.com>
2016-01-06 14:14:31 +0800

    super class has a static function to generate child class object

--- codes/php/test.php
+++ codes/php/test.php
@@ -1,2 +1,32 @@
 <?php
-echo 'test git rest';
+//it's commom for super class has a static function to generate child class object
+abstract class general{
+abstract function read();
+abstract function write();
+static function getIntance($atype){
+	if($atype)
+	return new atype;
+	else
+	return new btype;
+}
+}
+
+class atype extends general{
+	function write(){
+	echo "atype::write()";
+}
+	function read(){
+	echo "atype::read()";
+}
+}
+class btype extends general{
+	function write(){
+	echo "btype::write()";
+}
+	function read(){
+	echo "btype::read()";
+}
+}
+
+$x=general::getIntance(false);
+$x->write();
